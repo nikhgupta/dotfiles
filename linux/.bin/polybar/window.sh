39955fb3b88ceb1ba50f4afa491aa2c42708f08a
Nikhil Gupta <me@nikhgupta.com>
2019-06-19 00:16:35 +0530

    moved to archlinux

---
+++ linux/.bin/polybar/window.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ "$1" == "-i" ]; then
+    xdotool getactivewindow getwindowname 2>/dev/null | xclip -rmlastnl -selection c
+elif [ "$1" == "-c" ]; then
+  focused_window_id=$(xdotool getwindowfocus)
+  active_window_id=$(xdotool getactivewindow)
+  active_window_pid=$(xdotool getwindowpid "$active_window_id")
+  kill -9 $active_window_pid
+elif [ "$1" == "-w" ]; then
+  rofi -show window
+else
+    title=$(xdotool getactivewindow getwindowname 2>/dev/null || echo "ArchLinux")
+    [ "$title" == "Alacritty" ] && title='Terminal'
+    echo "$title"
+fi
---
+++ linux/.bin/session/session.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env zsh
+# Things to do when session starts.
+# Only useful for deepin DE. For others, should be configured
+# in ~/.xinitrc
+
+# add primary ssh key to agent
+eval $(ssh-agent)
+ssh-add ~/.ssh/nikhgupta
+
+if [[ "$XDG_SESSION_DESKTOP" == "deepin" ]]; then
+  killall -9 sxhkd compton dunst redshift syndaemon polybar
+  feh --bg-fill $XDG_WALLPAPER_DIR/_default.jpg
+  xrdb -merge ~/.Xdefaults
+
+  # set backlight to 25%
+  light -S 25
+
+  while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+  # polybar -q top &
+fi
+
