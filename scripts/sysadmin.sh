24e3a086b4e4edb50aad295258421dfc37e3aa94
Nikhil Gupta <me@nikhgupta.com>
2020-07-01 07:20:37 +0530

    made all scripts executable so that they can be run individually

--- scripts/sysadmin.sh
+++ scripts/sysadmin.sh
@@ -24,7 +24,8 @@ is_installed yay && yay
 
 highlight "Installing utilities and essential tools"
 pac_install curl wget make zsh vim tmux aria2 rclone ripgrep fd \
-    jq inotify-tools ncdu xclip ntfs-3g ttf-fira-code
+    jq inotify-tools ncdu xclip ntfs-3g ttf-fira-code timeshift \
+    kitty
 
 highlight "Installing dependencies for web dev setup"
 pac_install base-devel
