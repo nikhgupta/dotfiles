3fb197325e50ee9126129dd56b6e39d1c63b7a0b
Nikhil Gupta <me@nikhgupta.com>
2020-07-05 23:50:24 +0530

    updated lock screen to have better background blur using ffmpeg

--- rices/bspwm-fbars/.bin/screenlock.sh
+++ rices/bspwm-fbars/.bin/screenlock.sh
@@ -1,17 +1,33 @@
 #!/usr/bin/env bash
 
-ON_AC_TIMEOUT=60
-ON_BAT_TIMEOUT=30
+ON_AC_TIMEOUT=5
+ON_BAT_TIMEOUT=5
 
 source ~/.zsh/utils.sh
-if ! is_installed i3lock; then
-  error "Please, install i3lock-color to use $0"
+if ! is_installed i3lock || ! is_installed ffmpeg || ! is_installed xrandr; then
+  error "Please, install i3lock-color, ffmpeg and extra/xorg-xrandr to use $0"
 fi
 
-_font=Fira\ Code
+_image=/tmp/i3lock.png
+_text="${1:-Screen Locked}"
+_font="Fira Code"
+_font_path=/usr/share/fonts/TTF/FiraCode-Bold.ttf
+_lock=$DOTCASTLE/rices/$CURRENT_RICE/assets/lock.png
+_resolution=$(xrandr --current | grep '*' | uniq | awk '{print $1}')
+_boxsize=$(expr $(echo $_resolution | cut -d 'x' -f1) / 2)
+_boxblur=5
+_gblur_steps=1
+_gblur_sigma=5
+
+_filter="boxblur=$_boxblur"
+_filter="$_filter,gblur=sigma=${_gblur_sigma}:steps=${_gblur_steps},drawtext=fontfile=$_font_path"
+_filter="$_filter:text=$_text:fontcolor='#ffffffe0':fontsize=40:x=(w-tw)/2:y=(h/PHI)+th"
+_filter="$_filter:shadowcolor='#1d1f21d0':shadowx=2:shadowy=2"
+_filter="$_filter:box=1:boxcolor=#1a1b22@0.4:boxborderw=$_boxsize,overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2"
 
 lock() {
-  i3lock --ignore-empty-password --blur 10 --indicator --clock \
+  ffmpeg -f x11grab -video_size $_resolution -y -i $DISPLAY -i $_lock -filter_complex "$_filter" -vframes 1 $_image &>/dev/null
+  i3lock --ignore-empty-password --blur 10 --indicator \
     --pass-media-keys --pass-power-keys \
     --radius 110 \
     --color 666666 \
@@ -27,29 +43,29 @@ lock() {
     --keyhlcolor ffffffcc \
     --verifcolor ffffffff \
     --wrongcolor ccccccff \
-    --timecolor ffffffff \
-    --datecolor ffffffff \
-    --time-font $_font \
-    --date-font $_font \
     --verif-font $_font \
     --wrong-font $_font \
     --veriftext "..." \
     --wrongtext "" \
-    --noinputtext ""
-}
+    --noinputtext "" \
+    --locktext "" \
+    --lockfailedtext "" \
+    -i $_image
+    rm -f $image
+  }
 
-revert() { xset s off -dpms; }
+revert() { xset s off -dpms; rm -f $_image; }
 trap revert HUP INT TERM
 
-bat=$(acpi -a)
-if [ "$bat" = "Adapter 0: on-line" ]; then
-  lock &
-  while pgrep i3lock; do
-    sleep $ON_AC_TIMEOUT && pgrep i3lock && xset dpms force off
-  done
-  revert
+lock &
+
+if acpi -a | grep -q "off-line"; then
+  sleep ${ON_BAT_TIMEOUT:-60}s && pgrep i3lock && echo "Suspending in ${ON_BAT_TIMEOUT}s.." && systemctl suspend
 else
-  lock &
-  sleep $ON_BAT_TIMEOUT && pgrep i3lock && systemctl suspend
-  revert
+  while :; do
+    sleep ${ON_AC_TIMEOUT:-60}s && pgrep i3lock && echo "Screen off in ${ON_AC_TIMEOUT}s.." && xset dpms force off
+    pgrep i3lock || break
+  done
 fi
+
+revert
